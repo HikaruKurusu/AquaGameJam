76232d87716c214483007564b3cfd0e87e7efe0a
Zion <kodesone13@gmail.com>
2024-04-07 00:47:21 -0700

    Added music, background images, and pixel font

--- main.py
+++ main.py
@@ -6,20 +6,35 @@ screen = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("Menu")
 clock = pygame.time.Clock()
 running = True
+font_path = "Pixel.ttf"
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+# lines 11 - 13 for music
+bgMusic = "backgroundMusic.mp3"
+pygame.mixer.init()
+
 
 def get_font(size):
-    return pygame.font.SysFont("Times", size)
+    return pygame.font.Font(font_path, size)
+
+def play_music():
+    pygame.mixer.music.load(bgMusic)
+    pygame.mixer.music.play(-1)
 
 def play():
-    pygame.display.set_caption("Play")
-    bg = pygame.image.load("bg.jpg")
+    #pygame.display.set_caption("Play")
+    bg = pygame.image.load("Background.png")
+    bg = pygame.transform.scale(bg,(720, 720))
     while running:
         PLAY_MOUSE_POS = pygame.mouse.get_pos()
-        screen.blit(bg, (0, 0))
-        PLAY_TEXT = get_font(100).render("PLAY", True, "Black")
-        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 100))
-        screen.blit(PLAY_TEXT, PLAY_RECT)
-        PLAY_BACK = Button(pos=(640, 250), textInput="BACK", font=get_font(75), baseColor="White", hoveringColor="Black")
+        screen.blit(bg, (280,0))
+        #PLAY_TEXT = get_font(100).render("PLAY", True, "Black")
+        #PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 100))
+        #screen.blit(PLAY_TEXT, PLAY_RECT)
+        PLAY_BACK = Button(pos=(640, 650), textInput="BACK", font=get_font(25), baseColor="Black", hoveringColor="red")
+        #background_rect = pygame.rect(PLAY_BACK.rect.x-5, PLAY_BACK.rect.y, PLAY_BACK.rect.width, PLAY_BACK.rect.height)
+        pygame.draw.rect(screen,WHITE,(PLAY_BACK.rect.x-9, PLAY_BACK.rect.y+1, PLAY_BACK.rect.width+15, PLAY_BACK.rect.height))
+        pygame.draw.rect(screen,BLACK,(PLAY_BACK.rect.x-9, PLAY_BACK.rect.y+1, PLAY_BACK.rect.width+15, PLAY_BACK.rect.height),5)
         PLAY_BACK.changeColor(PLAY_MOUSE_POS)
         PLAY_BACK.update(screen)
         for event in pygame.event.get():
@@ -34,7 +49,7 @@ def play():
 
 def mainMenu():
     global running  # Declare 'running' as a global variable
-
+    
     pygame.display.set_caption("Menu")
     clock = pygame.time.Clock()
     running = True  # Initialize 'running' as True within the function scope
@@ -68,4 +83,6 @@ def mainMenu():
         pygame.display.update()
         clock.tick(60)
 
+
+play_music()
 mainMenu()
